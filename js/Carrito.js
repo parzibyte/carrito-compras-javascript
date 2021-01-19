589adf90e85c86d85d75c9f6520ee01567244708
Luis Cabrera Benito <parzibyte@gmail.com>
2021-01-18 20:34:09 -0600

    Créditos y mejoras menores

--- js/Carrito.js
+++ js/Carrito.js
@@ -1,3 +1,30 @@
+/*
+
+  ____          _____               _ _           _       
+ |  _ \        |  __ \             (_) |         | |      
+ | |_) |_   _  | |__) |_ _ _ __ _____| |__  _   _| |_ ___ 
+ |  _ <| | | | |  ___/ _` | '__|_  / | '_ \| | | | __/ _ \
+ | |_) | |_| | | |  | (_| | |   / /| | |_) | |_| | ||  __/
+ |____/ \__, | |_|   \__,_|_|  /___|_|_.__/ \__, |\__\___|
+         __/ |                               __/ |        
+        |___/                               |___/         
+    
+____________________________________
+/ Si necesitas ayuda, contáctame en \
+\ https://parzibyte.me               /
+ ------------------------------------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+Creado por Parzibyte (https://parzibyte.me).
+------------------------------------------------------------------------------------------------
+Si el código es útil para ti, puedes agradecerme siguiéndome: https://parzibyte.me/blog/sigueme/
+Y compartiendo mi blog con tus amigos
+También tengo canal de YouTube: https://www.youtube.com/channel/UCroP4BTWjfM0CkGB6AFUoBg?sub_confirmation=1
+------------------------------------------------------------------------------------------------
+*/
 class Carrito {
     constructor(clave) {
         this.clave = clave || "productos";
@@ -29,8 +56,7 @@ class Carrito {
     }
 
     existe(id) {
-        const productos = this.productos;
-        return productos.find(producto => producto.id === id);
+        return this.productos.find(producto => producto.id === id);
     }
 
     obtenerConteo() {
